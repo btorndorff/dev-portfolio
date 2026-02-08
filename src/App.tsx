865e34e2967b7eb1d103aaecc5fd7b5731006b5d
Benjamin Orndorff <benjaminorndorff@Benjamins-MacBook-Pro.local>
2026-02-07 18:03:59 -0800

    fixup

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function AppContent() {
     <div
       className={cn(
         "relative z-10 min-h-screen flex flex-col items-center pt-[15vh] overflow-hidden",
-        isDesktopPhotosRoute && "justify-end pointer-events-none",
+        isDesktopPhotosRoute && "justify-end pointer-events-none"
       )}
     >
       <AnimatePresence mode="wait">
@@ -56,17 +56,19 @@ function AppContent() {
           <Paper
             className={cn(
               isDesktopPhotosRoute &&
-                "!min-h-0 !h-fit after:!h-screen after:bottom-0 pointer-events-auto",
+                "!min-h-0 !h-fit after:!h-screen after:bottom-0 pointer-events-auto"
             )}
           >
             <div className="flex flex-col justify-between gap-8 flex-1">
-              <Header />
-              <Routes location={location}>
-                <Route path="/" element={<About />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/projects/:slug" element={<ProjectPage />} />
-                <Route path="/photos" element={<Photos />} />
-              </Routes>
+              <div className="flex flex-col gap-8">
+                <Header />
+                <Routes location={location}>
+                  <Route path="/" element={<About />} />
+                  <Route path="/projects" element={<Projects />} />
+                  <Route path="/projects/:slug" element={<ProjectPage />} />
+                  <Route path="/photos" element={<Photos />} />
+                </Routes>
+              </div>
               {!isDesktopPhotosRoute && <Footer />}
             </div>
           </Paper>
